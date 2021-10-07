5f77fdaa87f74c79ae1dc92ced0858f41623afd0
Jon Vegard Venås <jonvegard89@yahoo.com>
2021-10-07 14:01:23 +0200

    small change

--- models/setHetmaniukParameters.m
+++ models/setHetmaniukParameters.m
@@ -2,11 +2,11 @@ function layer = setHetmaniukParameters(noDomains)
 if nargin < 1
     noDomains = 2;
 end
-rho_f = [1000,1000]; % Density of outer fluid
+rho_f = 1000; % Density of outer fluid
 rho_s = 7850; % Density of solid
-c_f = [1500,1500];   % Speed of sound in outer (exterior) fluid domain
+c_f = 1500;   % Speed of sound in outer (exterior) fluid domain
 t = 0.05; % The thickness of the sphere
-R_o = [1,0]; % Outer radius of shell
+R_o = 1; % Outer radius of shell
 E = 2.0e11; % Youngs modulus of elastic material
 nu = 0.3; % Poisson ratio of elastic material
 
