7f1d53b929193d8acede0678be113336f05472ba
Zetison <jonvegard89@yahoo.com>
2022-04-21 21:36:06 +0200

    Generalize parpool

--- utils/startMatlabPool.m
+++ utils/startMatlabPool.m
@@ -1,18 +1,15 @@
 waringMessageId = 'MATLAB:mir_warning_maybe_uninitialized_temporary';
 warning('off',waringMessageId)
-if verLessThan('matlab', '8.3 (R2014a)')
-    isOpen = matlabpool('size') > 0;
-    if ~isOpen
-        matlabpool open 12
-        warning('Change the above number to match the number of workers available!')
-    end
-else
-    if ~exist('noCoresToUse','var')
-        noCoresToUse = feature('numCores');
-    end
-    poolobj = gcp('nocreate');
-    if isempty(poolobj)
-        noCoresToUse = min(feature('numCores'),noCoresToUse);
-        parpool(noCoresToUse, 'IdleTimeout', Inf)
-    end
+
+if ~exist('noCoresToUse','var')
+    noCoresToUse = feature('numCores');
+end
+poolobj = gcp('nocreate');
+if isempty(poolobj)
+    noCoresToUse = min(feature('numCores'),noCoresToUse);
+    parpool(noCoresToUse, 'IdleTimeout', Inf)
+elseif poolobj.NumWorkers ~= min(feature('numCores'),noCoresToUse)
+    delete(poolobj)
+    noCoresToUse = min(feature('numCores'),noCoresToUse);
+    parpool(noCoresToUse, 'IdleTimeout', Inf)
 end
