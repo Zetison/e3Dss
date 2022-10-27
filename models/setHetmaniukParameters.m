4d10f91a648a603aed115ea07d572c4cf7173b64
Jon Vegard Venås <jonvegard89@yahoo.com>
2022-10-27 14:07:16 +0200

    Extend for 3 domain case

--- models/setHetmaniukParameters.m
+++ models/setHetmaniukParameters.m
@@ -1,12 +1,12 @@
 function layer = setHetmaniukParameters(noDomains)
 if nargin < 1
-    noDomains = 2;
+    noDomains = 3;
 end
-rho_f = 1000; % Density of outer fluid
-c_f = 1500;   % Speed of sound in outer (exterior) fluid domain
+rho_f = [1000,1000]; % Density of outer fluid
+c_f = [1500,1500];   % Speed of sound in outer (exterior) fluid domain
 rho_s = 7850; % Density of solid
 t = 0.05; % The thickness of the sphere
-R_o = 1; % Outer radius of shell
+R_o = [1,0]; % Outer radius of shell
 E = 2.0e11; % Youngs modulus of elastic material
 nu = 0.3; % Poisson ratio of elastic material
 
