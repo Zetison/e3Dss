d4a919d1536c9f525119edb49b6b0fdd99801e28
Zetison <jonvegard89@yahoo.com>
2023-09-28 18:15:00 +0200

    Add comparison with Chang article

--- utils/T_.m
+++ utils/T_.m
@@ -5,15 +5,15 @@ switch tIdx
         T = -n*(n+1)*Z{i,1};
     case 2
         T = -(n+1)*Z{i,1} + eta.*g{i}.*Z{i,2};   
-    case 3
+    case 3 % -T_13
         T = -n*(n+1)*((n-1)*Z{i,1} - eta.*g{i}.*Z{i,2});   
     case 4
         T = (eta.^2-n^2+1).*Z{i,1} - eta.*g{i}.*Z{i,2};        
     case 5
         T = -n*(n+1)*((n-1)*Z{i,1} - eta.*g{i}.*Z{i,2});
-    case 6
+    case 6 % -T_33
         T = -n*(n+1)*Z{i,1};
-    case 7
+    case 7 % -T_43
         T = -(n^2-1-0.5*eta.^2).*Z{i,1} - eta.*g{i}.*Z{i,2}; 
     case 8
         T = n*(n+1)*((-n^2+3*n-2+eta.^2).*Z{i,1} - 4*eta.*g{i}.*Z{i,2});   
